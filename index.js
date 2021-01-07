b4b7c64ad01663989b847f400bb718e571c8f672
Nicolas Thebaud <nicolas.thebaud@wiztivi.com>
2021-01-07 13:56:17 +0100

    redone formats + updated API

--- index.js
+++ index.js
@@ -7,15 +7,48 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const customers = [
+
+];
+
 app.get('/getThemes', (request, response) => {
     console.warn('×', Aggregator.themes);
     response.json(Aggregator.themes);
 });
 
+app.get('/getCustomers', (request, response) => {
+    response.json(customers);
+});
+
 app.post('/getCustomerTheme', (request, response) => {
     const body = request.body;
+    let customer = customers.find(c => c.cpe === body.cpeID);
+    if (!customer) {
+        customer = { cpe: body.cpeID, themeId: Aggregator.map[0][body.cpeID] }
+        customers.push(customer);
+    }
+    const theme = Aggregator.themes.find(theme => theme.id === customer.themeId);
+    response.json(theme);
+});
+
+app.post('/setCustomerTheme', (request, response) => {
+    const body = request.body;
     console.warn('×', '[CPE ID]', body);
-    response.json(Aggregator.themes.find(theme => theme.id === Aggregator.map[0][body.cpeID]));
+    customers.find(c => c.cpe === body.cpe)['themeId'] = body.id;
+    console.warn('××', customers);
+});
+
+app.post('/setDisplayTokenValue', (request, response) => {
+    const body = request.body;
+    console.warn('×', '[SINGLE TOKEN]', body);
+    const theme = Aggregator.themes.find(theme => theme.id === body.themeId);
+    theme.style[body.args[0]] = body.args[1];
+});
+
+app.post('/setTokens', (request, response) => {
+    const body = request.body;
+    console.warn('×', '[SET TOKENS]', body);
+    Aggregator.themes[body.id] = body.tokens;
 });
 
 app.listen(1234);
